bc2fc4b30acf1622b9f723b69075b4eca32c38f4
chibuisi <amiaka.chibuisi@gmail.com>
2019-11-17 09:35:35 -0600

    init ui service. created dummy ui for home, login, welcome and user profile

--- ui-service/src/main/java/ea/finalproject/uiservice/config/AppSecurityConfig.java
+++ ui-service/src/main/java/ea/finalproject/uiservice/config/AppSecurityConfig.java
@@ -1,8 +1,5 @@
 package ea.finalproject.uiservice.config;
 
-import com.mum.userservice.filter.JwtRequestFilter;
-import com.mum.userservice.filter.UserFilter;
-import com.mum.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,41 +11,59 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private JwtRequestFilter filter;
+//
 //    @Autowired
-//    private UserFilter userFilter;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-    }
-
-//    @Bean
-//    public FilterRegistrationBean filterRegistrationBean(){
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(userFilter);
-//        filterRegistrationBean.addUrlPatterns("/url/*");
-//        filterRegistrationBean.setOrder(1);
-//        return filterRegistrationBean;
+//    private UserService userService;
+//    @Autowired
+//    private JwtRequestFilter filter;
+////    @Autowired
+////    private UserFilter userFilter;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService);
 //    }
+//
+////    @Bean
+////    public FilterRegistrationBean filterRegistrationBean(){
+////        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+////        filterRegistrationBean.setFilter(userFilter);
+////        filterRegistrationBean.addUrlPatterns("/url/*");
+////        filterRegistrationBean.setOrder(1);
+////        return filterRegistrationBean;
+////    }
+//
+//   @Bean
+//   public PasswordEncoder passwordEncoder(){
+//       return NoOpPasswordEncoder.getInstance();
+//   }
+//
+@Bean
+@Override
+public UserDetailsService userDetailsService() {
+    UserDetails user =
+            User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("password")
+                    .roles("USER")
+                    .build();
 
-   @Bean
-   public PasswordEncoder passwordEncoder(){
-       return NoOpPasswordEncoder.getInstance();
-   }
+    return new InMemoryUserDetailsManager(user);
+}
 
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
@@ -58,12 +73,22 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/user/authenticate").permitAll()
-                .antMatchers("/user/validate").permitAll()
+                .authorizeRequests().antMatchers("/login","/","/index").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        //http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
--- ui-service/src/main/java/ea/finalproject/uiservice/config/WebConfig.java
+++ ui-service/src/main/java/ea/finalproject/uiservice/config/WebConfig.java
@@ -1,4 +1,14 @@
 package ea.finalproject.uiservice.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public SpringSecurityDialect securityDialect(){
+        return new SpringSecurityDialect();
+    }
 }
